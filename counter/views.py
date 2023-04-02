e69d79163b161bf1890afdb9f38ca7dee977ccf6
Munachi <victorycollins31@gmail.com>
2023-04-02 09:23:40 +0100

    added my API key to the fetched API

--- counter/views.py
+++ counter/views.py
@@ -4,16 +4,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
+
     import requests
 
-query = '1lb brisket and fries'
-api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
-response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
-if response.status_code == requests.codes.ok:
-    print(response.text)
-else:
-    print("Error:", response.status_code, response.text)
-
+    query = '1lb brisket and fries'
+    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
+    response = requests.get(api_url, headers={'X-Api-Key': 'FPYaR6NqNCPNHD0BZEw/HA==ZyIuuL9csHHCnkyk'})
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print("Error:", response.status_code, response.text)
 
     return render(request, 'home.html')
     
